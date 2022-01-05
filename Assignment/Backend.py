bf4d945c5f4a5a874661dd28528d262557aa40fd
OmerWaizman <omezn289@gmail.com>
2022-01-06 00:02:41 +0200

    ex10

--- Assignment/Backend.py
+++ Assignment/Backend.py
@@ -1,87 +1,61 @@
-
-from flask import Flask, redirect, url_for
-from flask import render_template
-from flask import request
-from flask import session
-
+from flask import Flask, redirect, url_for, request, Blueprint
+from flask import render_template, session
+from pages.assignment10.assignment10 import assignment10
+from interact_with_DB import interact_db
 app = Flask(__name__)
-app.secret_key = '12321'
+app.register_blueprint(assignment10)
+app.secret_key = '123'
 
 
-@app.route('/home_page')
-@app.route('/home')
-@app.route('/')
-def home_func():
-    # DB
-    found = True
-    if found:
-        return render_template('index.html', name='Omer Waizman')
-    else:
-        return render_template('index.html')
 
-
-def hello_world():  # put application's code here
-    # TODO
-    return redirect(url_for('assignment8.html'))
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
 
 
 @app.route('/assignment8')
-def assignment8_func():
-    profile = {'Name': 'Omer', 'Last Name': 'Waizman'}
-    uni = 'bgu'
-    degrees = ['BSc']
-    hobbies = ('art', 'sport', 'music', 'sleep')
-    return render_template('Assignment8.html', fullname=profile, University=uni, degress=degrees, hobbies=hobbies)
+def assignment8_page():
+    return render_template('Assignment8.html', name='Omer Waizman' , sport1='football' ,sport2='runing' , prog='puyton' , prog2='JAVA', shows=('sainfeld' , 'how i met your mother' , 'suits' , 'survivor' , 'peaky blinders' , '24'))
 
-@app.route('/about')
-def about_func():
-    profile = {'Name': 'Omer', 'Last Name': 'Waizman'}
-    uni = 'bgu'
-    degrees = ['BSc']
-    hobbies = ('art', 'sport', 'music', 'sleep')
-    return render_template('about.html', fullname=profile, University=uni, degress=degrees, hobbies=hobbies)
+@app.route('/header')
+def header_page():
+    return render_template('Header.html')
+
+users = {'user1': {'name': 'Michael:', 'email':'Michael@gmail.com'},
+             'user2':{'name': 'Ido:', 'email':'Ido@gmail.com'},
+             'user3': {'name': 'Roni:', 'email': 'Roni@gmail.com'},
+             'user4': {'name': 'Omer:', 'email': 'Omer@gmail.com'},
+             'user5': {'name': 'Gil:', 'email': 'Gil@gmail.com'}}
+
+@app.route('/assignment9', methods=['GET','POST'])
+def search_9():
 
 
-@app.route('/assignment9', methods=['GET', 'POST'])
-def Assignment9_func():
-    users = {'user1': {'name': 'Or', 'email': 'Or@gmail.com'},
-             'user2': {'name': 'Tal', 'email': 'Tal@gmail.com'},
-             'user3': {'name': 'Dana', 'email': 'Dana@gmail.com'},
-             'user4': {'name': 'Omer', 'email': 'Omer@gmail.com'},
-             'user5': {'name': 'Nadav', 'email': 'Nadav@gmail.com'}}
+
     if request.method == 'GET':
-        if 'usersearch' in request.args:
-            search_user = request.args['usersearch']
-            return render_template('Assignment9.html', users=users, search_user=search_user)
-        return render_template('Assignment9.html', users=users)
-    if request.method == "POST":
-        username = request.form['username']
-        email = request.form['email']
-        session['username'] = username
-        session['email'] = email
+        if 'name' in request.args and 'email' in request.args and 'is_submit' in request.args:
+            name = request.args['name']
+            email = request.args['email']
+            is_submit = request.args['is_submit']
+
+
+
+            for value in users.values():
+                if(value.get("name") == name and value.get("email") == email):
+                    return render_template('Assignment9.html', name=name, email=email)
+
+
+            if(is_submit):
+                return render_template('Assignment9.html', users=users)
         return render_template('Assignment9.html')
 
 
-@app.route('/catalog')
-def catalog_func():
-    if 'product' in request.args:
-        product = request.args['product']
-        size = request.args['size']
-        return render_template('catalog.html', product=product, size=size)
-    return render_template('catalog.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login_fun():
-    if request.method == 'GET':
-        return render_template('login.html')
     if request.method == 'POST':
-        # DB
         username = request.form['username']
-        password = request.form['password']
-        session['username'] = username
-        session['password'] = password
-        return redirect(url_for('home_func'))
+        email = request.form['email']
+        if 'username' in request.form:
+            session['username'] = username
+            return render_template('Assignment9.html', username=username)
 
 
 @app.route('/logout')
@@ -90,5 +64,21 @@ def logout_func():
     return render_template('Assignment9.html')
 
 
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+# @app.route('/users')
+# def users_page():
+# query = 'select * from users':
+# users = interact_with_DB(query=query, query_type='fetch')
+# return render_template('users.html', users='users')
