9dfcb5540e229119e4d7059fa47e5fe3a74cc7e1
OmerWaizman <omezn289@gmail.com>
2022-01-14 22:13:05 +0200

    ex 11+12

--- Assignment/Backend.py
+++ Assignment/Backend.py
@@ -1,11 +1,17 @@
-from flask import Flask, redirect, url_for, request, Blueprint
+import json
+import requests
+import random
+from flask import Flask, redirect, url_for, request, Blueprint, jsonify
 from flask import render_template, session
-from pages.assignment10.assignment10 import assignment10
+from mysqlx.protobuf.mysqlx_resultset_pb2 import JSON
+import asyncio
+import aiohttp
+from pages.Assignment10.Assignment10 import Assignment10
 from interact_with_DB import interact_db
 app = Flask(__name__)
-app.register_blueprint(assignment10)
+app.register_blueprint(Assignment10)
 app.secret_key = '123'
-
+from flask import jsonify
 
 
 @app.route('/about')
@@ -39,7 +45,6 @@ def search_9():
             is_submit = request.args['is_submit']
 
 
-
             for value in users.values():
                 if(value.get("name") == name and value.get("email") == email):
                     return render_template('Assignment9.html', name=name, email=email)
@@ -64,21 +69,57 @@ def logout_func():
     return render_template('Assignment9.html')
 
 
+@app.route('/assignment11/outer_source')
+def assignment11_def():
+    return render_template('assignment11.html')
+@app.route('/assignment11/outer_source/json')
+def assignment11_def_json():
+    number = request.args['number']
+    res = requests.get("https://reqres.in/api/users/{}".format(number))
+    response = jsonify(res.json())
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+@app.route('/assignment11/users')
+def assignment11_users_def():
+        query = 'select  id,name,email from users;'
+        users = interact_db(query=query, query_type='fetch')
+        response = []
+        for user in users:
+            response.append({
+                "id": user[0],
+                "name": user[1],
+                "email": user[2]
+            })
+        return jsonify(users)
+        #return render_template('users.html', users=json.dumps(response))
 
+
+
+@app.route('/db_users/<int:user_id>')
+@app.route('/assignment12/restapi/', defaults={'user_id':1})
+@app.route('/assignment12/restapi/<int:user_id>')
+def get_users_def(user_id):
+    query = 'select  id,name,email from users where id=%s;' % user_id
+    users = interact_db(query=query, query_type='fetch')
+    user_dict = {
+        f'id': users[0].id,
+        'name': users[0].name,
+        'email': users[0].email
+    }
+    if len(users) == 0 :
+        user_dict = {
+            'status' : 'failed' ,
+            'message' : 'user not found'
+        }
+    else:
+        user_dict = {
+            'status': 'success',
+            f'id': users[0].id,
+            'name': users[0].name,
+            'email': users[0].email
+        }
+    return jsonify(user_dict)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
-
-# @app.route('/users')
-# def users_page():
-# query = 'select * from users':
-# users = interact_with_DB(query=query, query_type='fetch')
-# return render_template('users.html', users='users')
